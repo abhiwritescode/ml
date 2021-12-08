a0e3da9add5d3e2f4944a101c926cb9cbcbbd903
abhiwritescode <95482993+abhiwritescode@users.noreply.github.com>
2021-12-08 09:06:27 -0500

    Update validation_test_cal.py

--- validation_test_cal.py
+++ validation_test_cal.py
@@ -18,3 +18,46 @@ train_df = pd.read_csv(filepath_or_buffer="housing_train_data.csv")
 train_df["median_house_value"] /= 1000.0
 
 test_df = pd.read_csv(filepath_or_buffer="housing_test_data.csv")
+
+# The following variables are the hyperparameters.
+learning_rate = 0.08
+epochs = 30
+batch_size = 100
+
+# Split the original training set into a reduced training set and a
+# validation set. 
+validation_split = 0.2
+
+# Identify the feature and the label.
+my_feature = "median_income"    # the median income on a specific city block.
+my_label = "median_house_value" # the median house value on a specific city block.
+# That is, you're going to create a model that predicts house value based 
+# solely on the neighborhood's median income.  
+
+# Discard any pre-existing version of the model.
+my_model = None
+
+# Invoke the functions to build and train the model.
+my_model = build_model(learning_rate)
+epochs, rmse, history = train_model(my_model, train_df, my_feature, 
+                                    my_label, epochs, batch_size, 
+                                    validation_split)
+
+plot_the_loss_curve(epochs, history["root_mean_squared_error"], 
+                    history["val_root_mean_squared_error"])
+
+shuffled_train_df = train_df.reindex(np.random.permutation(train_df.index))
+
+epochs, rmse, history = train_model(my_model, shuffled_train_df, my_feature, 
+                                      my_label, epochs, batch_size, 
+                                      validation_split)
+
+
+## Evaluate using test data
+x_test = test_df[my_feature]
+y_test = test_df[my_label]
+
+results = my_model.evaluate(x_test, y_test, batch_size=batch_size)
+
+
+
